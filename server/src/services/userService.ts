ebf2831ed528d63046105be4fd36776ad714b8ba
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-05-28 10:28:31 +0200

    updates to mongodb to use compass

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -5,6 +5,7 @@ class UserService {
     async createUser(userData: IUser) {
         //TODO write logic here to check if user already exists, hash pass, send confirmation email on signup? 
         try {
+            
             const user = await userRepository.create(userData)
             console.log(user)
             return user
