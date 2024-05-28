c073d50b496e525821d84fc861d480f15c8d1826
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-05-28 16:17:26 +0200

    Remove address data

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -3,8 +3,6 @@ import { Schema, model, Document } from "mongoose";
 interface IUser extends Document {
     firstName: string;
     lastName: string;
-    // TODO: is address necessary? / address interface? 
-    address: object,
     email: string;
     password: string;
     role: string
@@ -13,7 +11,6 @@ interface IUser extends Document {
 const UserSchema = new Schema<IUser>({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    address: {type: Object, required: true},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, required: true }
