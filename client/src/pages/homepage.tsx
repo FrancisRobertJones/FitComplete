90266a111486943dd0450615bcb1c591ab3afe54
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-05-28 19:59:58 +0200

    starting logic for reusable card component

--- client/src/pages/homepage.tsx
+++ client/src/pages/homepage.tsx
@@ -7,9 +7,9 @@ const Homepage = () => {
                      <Navbar />
                      <h1 className='text-8xl mb-24'>THIS IS HOME</h1>
                      <div className='flex flex-col items-center'> 
-                        <Homepagecard />
-                        <Homepagecard />
-                        <Homepagecard />
+                        <Homepagecard title={"Basic"} description={"Ideal for getting started"} linkUrl={'./levels/basic.tsx'} features={["Workouts"]}/>
+                        <Homepagecard title={"Medium"} description={"For people looking for more"} linkUrl={'./levels/basic.tsx'} features={["Workouts", "Videos"]}/>
+                        <Homepagecard title={"Premium"} description={"The full package"} linkUrl={'./levels/basic.tsx'} features={["Workouts", "Videos", "Nutrition"]}/>
                     </div>
     </>
   )
