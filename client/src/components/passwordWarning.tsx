c18c6baa7b588e019a8ed06d7bea53ddb501644d
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-05-29 12:16:02 +0200

    authcontext, login working as intended

---
+++ client/src/components/passwordWarning.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Badge } from "@/components/ui/badge"
+
+
+const PasswordWarning = () => {
+        return <Badge className='ml-12' variant="destructive">Passwords don't match</Badge>
+}
+
+export default PasswordWarning
