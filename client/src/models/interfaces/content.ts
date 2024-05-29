9bd74b463e8d8102efe460bd169bab3313822947
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-05-29 17:59:40 +0200

    Fetch data from DB & add basic design (temporary) to the basic page

---
+++ client/src/models/interfaces/content.ts
@@ -0,0 +1,8 @@
+export interface IWorkout {
+  id: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+  status: string;
+  level: number;
+}
--- client/src/pages/levels/basic.tsx
+++ client/src/pages/levels/basic.tsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import { ButtonSecondary } from "@/components/buttonSecondary";
+import { IWorkout } from "@/models/interfaces/content";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Basic = () => {
-    return (
-        <>
-            <div>THIS IS BASIC PAGE</div>
-            <button>SUBSCRIBE</button>
-        </>
-    )
-}
+  const [contents, setContents] = useState<IWorkout[]>([]);
 
-export default Basic
+  useEffect(() => {
+    const fetchContents = async () => {
+      const response = await axios.get(
+        "http://localhost:3000/content?type=workouts"
+      );
+      setContents(response.data);
+    };
+    fetchContents();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-5xl">THIS IS BASIC PAGE</h1>
+      <h2 className="text-3xl">All workouts</h2>
+      <div className="flex flex-wrap gap-4">
+        {contents.map((content) => (
+          <div
+            key={content.id}
+            className="relative w-80 h-64 rounded-xl overflow-hidden bg-cover bg-center bg-[url('/src/assets/_29082154-8468-478d-baca-283c04521995.jpg')]"
+          >
+            <div className="absolute bottom-0 bg-black bg-opacity-50 w-full p-4 text-white">
+              <h3 className="">{content.title}</h3>
+              <p className="">{content.description}</p>
+              {/* <img src={content.imageUrl} alt={content.title} /> */}
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* <ButtonSecondary text={"Subscribe"} url={""} height={"Medium"} /> */}
+    </div>
+  );
+};
+
+export default Basic;
