2f8013e68cd3a1253e9018a8cb4f857a42d75e9e
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-05-29 06:59:01 +0200

    testing with flex horizontal layout

--- client/src/pages/homepage.tsx
+++ client/src/pages/homepage.tsx
@@ -7,10 +7,10 @@ const Homepage = () => {
     <>
       <Navbar />
       <div className='px-32 py-12'>
-        <h1 className='text-8xl mb-24'>THIS IS HOME</h1>
+        <h1 className='text-8xl mb-12'>THIS IS HOME</h1>
         <div className='flex justify-content'>
           <AuthComponent />
-          <div className='flex flex-col items-center w-full'>
+          <div className='flex flex-wrap justify-between items-center w-full gap-4 pl-16'>
             <Homepagecard title={"Basic"} description={"Ideal for getting started"} linkUrl={'./levels/basic.tsx'} features={["Workouts"]} />
             <Homepagecard title={"Medium"} description={"For people looking for more"} linkUrl={'./levels/basic.tsx'} features={["Workouts", "Videos"]} />
             <Homepagecard title={"Premium"} description={"The full package"} linkUrl={'./levels/basic.tsx'} features={["Workouts", "Videos", "Nutrition"]} />
