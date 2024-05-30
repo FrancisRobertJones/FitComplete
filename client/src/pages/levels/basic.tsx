6a092b5427d9a65ca7a2c5cf0bcb2e2312123532
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-05-30 09:38:29 +0200

    Added small details

--- client/src/pages/levels/basic.tsx
+++ client/src/pages/levels/basic.tsx
@@ -14,24 +14,25 @@ const Basic = () => {
       setContents(response.data);
     };
     fetchContents();
-  }, []);
+  }, [contents]);
 
   return (
-    <div>
+    <div className="flex flex-col items-center gap-10">
       <h1 className="text-5xl">THIS IS BASIC PAGE</h1>
       <h2 className="text-3xl">All workouts</h2>
-      <div className="flex flex-wrap gap-4">
+      <div className="flex flex-wrap gap-12 px-10">
         {contents.map((content) => (
-          <div
+          <a
             key={content.id}
-            className="relative w-80 h-64 rounded-xl overflow-hidden bg-cover bg-center bg-[url('/src/assets/_29082154-8468-478d-baca-283c04521995.jpg')]"
+            href=""
+            className="relative w-80 h-64 rounded-xl overflow-hidden bg-cover bg-center grayscale bg-[url('/src/assets/_29082154-8468-478d-baca-283c04521995.jpg')] duration-200 hover:scale-110 hover:grayscale-0"
           >
             <div className="absolute bottom-0 bg-black bg-opacity-50 w-full p-4 text-white">
               <h3 className="">{content.title}</h3>
               <p className="">{content.description}</p>
               {/* <img src={content.imageUrl} alt={content.title} /> */}
             </div>
-          </div>
+          </a>
         ))}
       </div>
       {/* <ButtonSecondary text={"Subscribe"} url={""} height={"Medium"} /> */}
