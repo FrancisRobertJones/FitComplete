e5e576132e7d4d31c64409c617a71432accaed6a
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-06-03 14:46:12 +0200

    updates to data fetching

--- client/src/models/classes/Exercises.ts
+++ client/src/models/classes/Exercises.ts
@@ -10,11 +10,22 @@ export class NewExercise {
 
 export class WorkoutExercise{
     constructor(
-        public id: string | undefined,
+        public exerciseId: string | undefined,
         public name: string,
         public type: string,
         public reps: string,
         public sets: string,
         public duration?: string | null,
+    ) {}
+}
+
+export class ExerciseFromDB {
+    constructor(
+        public _id: string | undefined,
+        public type: string,
+        public name: string,
+        public instructions: string[],
+        public videoUrl: string,
+        public description: string
     ) {}
 }
