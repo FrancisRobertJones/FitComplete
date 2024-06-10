a415f2ef22576f2f2c4c6dadbae4088ef2ece15a
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-06-10 10:45:27 +0200

    small commit changed link from successpage home

--- client/src/pages/payment-successful.tsx
+++ client/src/pages/payment-successful.tsx
@@ -102,7 +102,7 @@ export const PaymentSuccessful = () => {
           </div>
           <div className="mt-8 flex justify-center">
             <Link
-              to={"#"}
+              to={"/"}
               className="inline-flex items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-white shadow-sm transition-colors hover:bg-gray-900/90 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus:ring-gray-300"
             >
               Go to Homepage
